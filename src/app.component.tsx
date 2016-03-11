7f70774569c50383ed42a86248be0facffe50aa7
YuanGang <262013@qq.com>
2016-03-11 16:02:31 +0800

    add source

---
+++ src/app.component.tsx
@@ -0,0 +1,17 @@
+/**
+ * Created by Gene on 16/3/11.
+ */
+/// <reference path="../typings/tsd.d.ts" />
+
+import { Router, Route, Link, browserHistory } from 'react-router';
+
+class AppComponent extends React.Component<any, any> {
+
+
+
+    render() {
+        return <div>Hello {"啊!五环!"}</div>
+    }
+}
+
+export default AppComponent;
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+/**
+ * Created by Gene on 16/3/11.
+ */
+
+/// <reference path="../typings/tsd.d.ts" />
+
+import AppComponent from './app.component'
+
+ReactDOM.render(<AppComponent name="a!wuhuan!" />, document.getElementById('container'));
+
+
+
+
+
