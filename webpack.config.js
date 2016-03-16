42f432cb1141396a50b1af3860dc12f0cd016a11
YuanGang <262013@qq.com>
2016-03-16 20:49:29 +0800

    add chapter_five

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ module.exports = {
         'chapter_one' : './build/chapter_one/chapter_one.js',
         'chapter_two' : './build/chapter_two/chapter_two.js',
         'chapter_three': './build/chapter_three/chapter_three.js',
-        'chapter_four': './build/chapter_four/chapter_four.js'
+        'chapter_four': './build/chapter_four/chapter_four.js',
+        'chapter_five': './build/chapter_five/chapter_five.js'
     },
     output: {
         path: './release/dist',
