f74687209ad9535f17c55a6b4e85156f68311329
YuanGang <262013@qq.com>
2016-03-18 15:25:58 +0800

    delete intaface

--- src/chapter_two/SearchBar.tsx
+++ src/chapter_two/SearchBar.tsx
@@ -4,11 +4,12 @@
 
 class SearchBar extends React.Component<any, any> {
     render() {
+        const { filterText, inStockOnly } = this.props;
         return (
             <form>
-                <input type="text" placeholder="Search..." />
+                <input type="text" placeholder="Search..." value={filterText}/>
                 <p>
-                    <input type="checkbox" />
+                    <input type="checkbox" checked={inStockOnly}/>
                     {' '}
                     Only show products in stock
                 </p>
