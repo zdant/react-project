6f3f4758fee05fac3ecea4d8188c6c0f8cbdee90
YuanGang <262013@qq.com>
2016-03-18 15:26:34 +0800

    delete intaface

--- src/chapter_two/FilterableProductTable.tsx
+++ src/chapter_two/FilterableProductTable.tsx
@@ -7,13 +7,18 @@ import ProductTable from './ProductTable'
 
 class FilterableProductTable extends React.Component<any, any> {
 
+    constructor(props) {
+        super(props);
+        this.state = {'filterText':'',inStockOnly:false}
+    }
 
     render() {
         const {products} = this.props;
 
         return (
             <div>
-                <SearchBar />
+                <SearchBar filterText={this.state.filterText}
+                           inStockOnly={this.state.inStockOnly}/>
                 <ProductTable products={products} />
             </div>
         )
